64d8b00b93614f0130e5d4e5842a7b1617c4afc4
nsouzaco <nsouzaco@asu.edu>
2025-10-28 10:40:18 -0700

    refactor: Remove play button from Timeline header
    Removed the redundant play/pause button next to 'Timeline' text.
    Playback controls are already available in the VideoPreview component,
    so the Timeline play button was unnecessary.
    
    Changes:
    - Removed play button from Timeline header
    - Removed unused togglePlay function
    - Cleaner Timeline header with just title and zoom control

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -361,10 +361,6 @@ export const Timeline: React.FC = () => {
     }
   };
 
-  const togglePlay = () => {
-    useAppStore.getState().setIsPlaying(!isPlaying);
-  };
-
   const handleZoomChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newZoom = parseFloat(e.target.value);
     setZoomLevel(newZoom);
@@ -378,20 +374,6 @@ export const Timeline: React.FC = () => {
       <div className="flex items-center justify-between p-4 pb-2 flex-shrink-0">
         <div className="flex items-center space-x-3">
           <h3 className="text-lg font-semibold text-white">Timeline</h3>
-          <button
-            onClick={togglePlay}
-            className="bg-blue-600 hover:bg-blue-700 text-white p-1 rounded transition-colors"
-          >
-            {isPlaying ? (
-              <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-              </svg>
-            ) : (
-              <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
-              </svg>
-            )}
-          </button>
         </div>
 
         <div className="flex items-center space-x-3">
